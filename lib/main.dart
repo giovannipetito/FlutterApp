4582d9d6035db161814ff58bad9df5685d345acf
Giovanni <giovanni.petito88@gmail.com>
2021-03-12 17:25:41 +0100

    implements Google Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter_app/bmi_calculator/bmi_main_screen.dart';
 import 'package:flutter_app/meteo/loading_screen.dart';
 import 'package:flutter_app/screens/price_screen.dart';
@@ -32,7 +35,40 @@ Future <void> main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    handleCrashlytics();
+  }
+
+  void handleCrashlytics() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    if (kDebugMode) {
+      // Force disable Crashlytics collection while doing every day development.
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+    } else {
+      // Handle Crashlytics enabled status when not in Debug,
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+      // Pass all uncaught errors from the framework to Crashlytics.
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+      // You can additionally read the current collection enabled status:
+      if (FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        // Collection is enabled.
+      }
+    }
+    // FirebaseCrashlytics.instance.crash(); // To force a crash.
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,7 +76,7 @@ class MyApp extends StatelessWidget {
       // all'avvio dell'app, ma possiamo indicare la prima schermata anche nel modo seguente:
       initialRoute: HomePage.id,
       // theme: ThemeData.dark(),
-      
+
       theme: ThemeData.light(),
       routes: {
         HomePage.id: (context) => HomePage(),
